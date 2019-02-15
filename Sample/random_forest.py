f6711bfbdf3e51f4fd86c5d79993c7b631c1be85
Pavle Ciric <pavle.ciric@hotmail.com>
2019-02-15 18:10:18 +0100

    [add] random_forest.py
    created random forest file and class

---
+++ Sample/random_forest.py
@@ -0,0 +1,14 @@
+import decision_tree as dt
+
+class Forest:
+    '''Class that represents the random forest. Contains an array of decision trees.'''
+    def __init__(self):
+        self.trees = []
+
+
+def build_forest(rows):
+    '''Builds the forest. 
+
+    Creates deciosion node classes and appentds
+    them into an array defined int Forest class.'''
+    pass
