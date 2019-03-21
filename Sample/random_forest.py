5f60658d8e898702b4b8ae4aadcba86916a0c970
Pavle Ciric <pavle.ciric@hotmail.com>
2019-03-21 20:14:21 +0100

    [edit]  Bootstrapped dataset funcion
    Out-of-bag samples are now sored in a new dict. The out-of-bag and the bootstraped dicts are converted into DataFrame object and returned.

--- Sample/random_forest.py
+++ Sample/random_forest.py
@@ -25,7 +25,21 @@ def buil_bootstrapped_dataset(rows):
 
     orig_dataset = rows.to_dict('indexed') # Covner the dataframe into a dict
     bootdata = {} #Create empty dict to store the randomly selected row from the original dataset
+    out_of_bag_samples = {}
 
+    # Creating bootstrapped dateset
     for i in range(len(orig_dataset)):
         rand_index = rnd.randint(0, len(rows.index)-1) #Chose a random int from 0 to the number of rows in the original dataset
         bootdata[i] = orig_dataset[rand_index] #Takes a random row from the original dataset and places it into do bootstraped datset
+
+    #Creating "out of bag dataset"
+    index = 0 # Counts next dict key
+    for value in orig_dataset.values(): #Interates all values form the original dataset
+        if value not in bootdata.values(): # Cheks if the value is in the bootstrapped dataset
+            out_of_bag_samples[index] = value # If it is not, then the value is placed in the "out of bag" dataset
+            index += 1
+
+    df_bootstrapped = pd.DataFrame.from_dict(bootdata, orient='index') # Converts dict to pd.DataFrame
+    df_out_of_bag = pd.DataFrame.from_dict(out_of_bag_samples, orient='index') # Converts dict to pd.DataFrame
+
+    return df_bootstrapped, df_out_of_bag # Return the new datasets
