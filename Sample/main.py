dc47256ce3632aeaeef170132887cfc2262d89e3
Pavle Ciric <pavle.ciric@hotmail.com>
2019-03-26 09:29:42 +0100

    edit edit edit

--- Sample/main.py
+++ Sample/main.py
@@ -6,13 +6,13 @@ def main():
     data = pd.read_csv('sample_dataset.csv')
     
     tree = dt.build_tree(data.values)
-    dt.print_tree(tree)
+    #dt.print_tree(tree)
     #print(data.values)
-    print(data.values[15])
-    prediction = tree.check_row(data.values[15]).keys()
+    #print(data.values[15])
+    #prediction = tree.check_row(data.values[15]).keys()
     
-    for key in prediction:
-        print(key)
+    #for key in prediction:
+       # print(key)
 
 if __name__ == '__main__':
     main()
