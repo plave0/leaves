02ad2bd80fa39a5e95d9a5a9ee6d5f73e951936d
Pavle Ciric <pavle.ciric@hotmail.com>
2019-04-26 12:19:47 +0200

    [add] open and show an image
    Function show_image in fex.py is used to display an image

--- fex.py
+++ fex.py
@@ -1,4 +1,10 @@
 '''Feature extraxtion (fe-ex)'''
+import cv2
 
 def find_edge():
     pass
+
+def show_image(image, title = "img"):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -2,18 +2,13 @@ import classification.decision_tree as dt
 import classification.random_forest as rf
 import fex
 import pandas as pd
+import cv2
 
 def main():
     '''Main program funcion
     This is just a testing function'''
-    
-    data = pd.read_csv('sample_dataset.csv')
-    #print(data.values[1])
-    forest = rf.build_forest(data)
-    #rf.print_forest(forest)
-    row = data.values[15]
-    print(row)
-    print(forest.check_row(row))
+    img = cv2.imread('samples\\sample.jpg', 1)
+    fex.show_image(img, 'sample')
 
 if __name__ == '__main__':
     main()
