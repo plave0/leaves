e41fa0ef7b744a72e894d7051960e2f4910f30ae
Pavle Ciric <pavle.ciric@hotmail.com>
2019-05-14 10:35:36 +0200

    [edit] all functions
    Added comments
    Edited rectangle and circle functions

--- image/fex.py
+++ image/fex.py
@@ -24,17 +24,23 @@ def find_thresh(image):
     return thresh
 
 def find_cnt(image):
+
+    #Find contour
     thr = find_thresh(image)
     edge = find_edge(thr)
     cnt,_ = cv2.findContours(edge, 1, 2)
 
+    #Draw contour
     new_img = np.zeros((edge.shape[0], edge.shape[1], 3), dtype = np.uint8)
     cv2.drawContours(new_img, cnt, 0, (255, 255, 0))
 
+    #Returns drawn contour and the contour object
     return new_img, cnt
 
 def find_hull(image):
     '''Returns image of the convex hull.'''
+
+    #Find image contour
     img, cnt = find_cnt(image)
 
     new_img = np.zeros((img.shape[0], img.shape[1], 3), dtype = np.uint8) #Create a blank image (array of zeros)
@@ -55,38 +61,82 @@ def find_hull(image):
 
 def find_rect(image):
     '''Finds the smallest bounding rectangle of a leaf.'''
+
+    #Find leaf convex hull
     _, hull = find_hull(image)
 
+    #Generate rectangle
     rect = cv2.minAreaRect(hull)
+    x , y = rect[0]
     box = cv2.boxPoints(rect)
     box = np.int0(box)
-    new_img = np.ones((2000, 2000, 3), dtype=np.uint8)
-    cv2.drawContours(new_img, [box], 0, (0,0,256), 2)
-    #Returns the image of the rect and the cnt
-    return new_img, box
+    
+    #Fing the x cordinate of the point with the lowest x cordinate
+    #and the y cordinate of the point with the lowest y cordinate
+    min_x_point = box[0][0]
+    min_y_point = box[0][1]
+    for point in box:
+        if point[0] < min_x_point:
+            min_x_point = point[0]
+        if point[1] < min_y_point:
+            min_y_point = point[1]
+
+    #Move center so all point are positive
+    if min_x_point < 0:
+        x += (abs(min_x_point) + 50)
+    if min_y_point < 0:
+        y += (abs(min_y_point) + 50)
+
+    #Create new rectangle
+    new_rect = ((x,y), rect[1], rect[2])
+    shape = list(new_rect[1])
+    box = cv2.boxPoints(new_rect)
+    box = np.int0(box)
+
+    #Draw rectangle
+    max_x_point = box[0][0]
+    max_y_point = box[0][1]
+    for point in box:
+        if point[0] > max_x_point:
+            max_x_point = point[0]
+        if point[1] > max_y_point:
+            max_y_point = point[1]
+    color = (0, 0, 255)
+    img_size = (int(max_y_point)+20, int(max_x_point+20)+20, 3)
+    rect_img = np.ones(img_size, dtype=np.uint8)
+    cv2.drawContours(rect_img, [box], 0, color, 2)
+
+    #Returns the image of the rectangle 
+    #and the rect object that contains all info about the rectangle (ceter, height, width, rotation)
+    return rect_img, rect
 
 def find_encl(image):
+    '''Finds the smallest enclosing circle of a leaf.'''
+
+    #Find leaf convex hull
     _, hull = find_hull(image)
+
+    #Generate circle
     (x,y),radius = cv2.minEnclosingCircle(hull)
+    while x-radius < float(0):
+        x+=50
+    while y-radius < float(0):
+        y+=50
+    
     center = (int(x),int(y))
     radius = int(radius)
-    new_img = np.ones((2000, 2000, 3), dtype=np.uint8)
-    cv2.circle(new_img,center,radius,(255,255,255),2)
-    _,cnt = find_cnt(new_img)
-    new_img = np.ones((2000, 2000, 3), dtype=np.uint8)
-    cv2.drawContours(new_img, [cnt[0]], 0, (0, 0, 256), 2)
+    
+    #Draw circle
+    color = (255,255,255)
+    img_size = (int(x + radius + 100), int(y + radius + 100), 3)
+    new_img = np.ones(img_size, dtype=np.uint8)
+    cv2.circle(new_img,center,radius, color,-1)
+
+    #Find circle contour
+    _, hull = find_hull(new_img) 
+
     #Returns the image of the circ and the cnt
-    return new_img, cnt
-
-def calc_area(image):
-    '''Returns the area of a leaf.'''
-    bw = thresh(image)
-    return cv2.countNonZero(bw)
-
-def calc_ciric(image):
-    '''Returns the circumference of the leaf.'''
-    edge = find_edge(image)
-    return cv2.countNonZero(edge)
+    return new_img, hull
 
 def resize_image(image, factor):
     '''Returns the resized image.
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import cv2
 def main():
     '''Main program funcion
     This is just a testing function'''
-    img = cv2.imread('samples\\sample_1.jpg', 1)
+    img = cv2.imread('samples\\sample_2.jpg', 1)
     #edge = fex.find_edge(img)
-    cntimage,_ = f.find_cnt(img)
+    cntimage, _ = f.find_encl(img)
     resized = f.resize_image(cntimage, 0.5)
     f.show_image(resized)
 
