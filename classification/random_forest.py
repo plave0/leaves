c7c0e15ab5746ba9e483ba1ca1f8e65e06f4a90f
plave <pavle.ciric@hotmail.com>
2019-07-08 19:40:08 +0200

    [fix] bootstraped dataset function
    now takes less time to run.

--- classification/random_forest.py
+++ classification/random_forest.py
@@ -47,27 +47,10 @@ def buil_bootstrapped_dataset(rows):
     It is built by taking random row from the original dataset and placing them in the bootstrapped dataset.
     The bootstrapped data set has the same number of rows as the original dataset.'''
 
-    orig_dataset = rows.to_dict('indexed') # Covner the dataframe into a dict
-    bootdata = {} #Create empty dict to store the randomly selected row from the original dataset
-    out_of_bag_samples = {}
-
-    # Creating bootstrapped dateset
-    for i in range(len(orig_dataset)):
-        #Chose a random int from 0 to the number of rows in the original dataset
-        rand_index = rnd.randint(0, len(rows.index)-1) 
-        #Takes a random row from the original dataset and places it into do bootstraped datset
-        bootdata[i] = orig_dataset[rand_index] 
-
-    #Creating "out of bag dataset"
-    index = 0 # Counts next dict key
-    for value in orig_dataset.values(): #Interates all values form the original dataset
-        if value not in bootdata.values(): # Cheks if the value is in the bootstrapped dataset
-            out_of_bag_samples[index] = value # If it is not, then the value is placed in the "out of bag" dataset
-            index += 1
-
-    df_bootstrapped = pd.DataFrame.from_dict(bootdata, orient='index') # Converts dict to pd.DataFrame
-    df_out_of_bag = pd.DataFrame.from_dict(out_of_bag_samples, orient='index') # Converts dict to pd.DataFrame
-
+    df_bootstrapped = rows.sample(n=len(rows.values),replace=True, random_state=1)
+    df_out_of_bag = pd.concat([rows, df_bootstrapped]).drop_duplicates(keep=False)
+    
+    
     return df_bootstrapped, df_out_of_bag # Return the new datasets
 
 def calc_num_on_trees(factor, num_of_cols):
