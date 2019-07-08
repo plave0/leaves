d59db2b246374bc88713dcc07d0faac269b1c55e
plave <pavle.ciric@hotmail.com>
2019-07-08 17:41:00 +0200

    [add] profiler

--- main.py
+++ main.py
@@ -7,20 +7,41 @@ import pandas as pd
 import cv2
 from pathlib import Path
 import os
+import cProfile, pstats, io
 
+def profile(fnc):
+    
+    """A decorator that uses cProfile to profile a function"""
+    
+    def inner(*args, **kwargs):
+        
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
+
+@profile
 def main():
     '''Main program funcion
     This is just a testing function'''
-    path = str(Path('samples/1143.jpg'))
-    img = cv2.imread(path, 1)
 
-    rows = pd.read_csv(Path(os.path.pardir, 'petnica-leaves/samples/dataset.csv').absolute())
+    rows = pd.read_csv(Path(os.path.pardir, 'petnica-leaves/samples/sample_dataset.csv').absolute())
     forest = rf.build_forest(rows,2)
     #rf.print_forest(forest)
     #row = rows.values[0]
     print(forest.calc_accu())
     #print(forest.check_row(row))
     #d.create_dataset()
+
+
     
 
 if __name__ == '__main__':
